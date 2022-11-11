b89f4b5dcb15ec5f21fd8615aa704c63c0445b56
Yaroslav Fedunkiv <ya.fedunkiv@gmail.com>
2022-11-11 09:28:20 +0200

    first commit

---
+++ src/main/java/epam/com/periodicals/PeriodicalsHwApplication.java
@@ -0,0 +1,13 @@
+package epam.com.periodicals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodicalsHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeriodicalsHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/epam/com/periodicals/PeriodicalsHwApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.com.periodicals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriodicalsHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
